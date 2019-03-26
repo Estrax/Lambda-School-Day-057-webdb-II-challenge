edbd1a0d3ad6cc74cb0450f9755bf664df4ab199
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-26 15:59:16 -0400

    added stretch - bears

--- index.js
+++ index.js
@@ -53,6 +53,44 @@ server.route('/api/zoos/:id')
       .catch(err => res.status(500).json({ error: "Some useful error message" }));
   });
 
+
+server.route('/api/bears')
+  .get(async (req, res) => {
+    db('bears')
+      .then(bears => res.status(200).json(bears))
+      .catch(err => res.status(500).json({ error: "Some useful error message" }))
+  })
+  .post(async (req, res) => {
+    if(!req.body.name) return res.status(400).json({ error: "Some useful error message" });
+    db('bears')
+      .insert(req.body)
+      .then(bear => res.status(201).json(bear))
+      .catch(err => res.status(500).json({ error: "Some useful error message" }));
+  });
+
+server.route('/api/bears/:id')
+  .get(async (req, res) => {
+    db('bears')
+      .where('id', req.params.id)
+      .then(bear => res.status(200).json(bear))
+      .catch(err => res.status(500).json({ error: "Some useful error message" }));
+  })
+  .put(async (req, res) => {
+    if(!req.body.name) return res.status(400).json({ error: "Some useful error message" });
+    db('bears')
+      .where('id', req.params.id)
+      .update(req.body)
+      .then(bear => res.status(200).json(bear))
+      .catch(err => res.status(500).json({ error: "Some useful error message" }));
+  })
+  .delete(async (req, res) => {
+    db('bears')
+      .where('id', req.params.id)
+      .del()
+      .then(bear => res.status(200).json(bear))
+      .catch(err => res.status(500).json({ error: "Some useful error message" }));
+  });
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
