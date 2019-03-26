d98ca227199a7ab4deccb5b5ae280b0f7fd3dfb8
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-26 15:17:47 -0400

    imported libraries and updated middleware

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors());
 
 // endpoints here
+
+server.route('/api/zoos')
+  .get(async (req, res) => {
+
+  })
 
 const port = 3300;
 server.listen(port, function() {
